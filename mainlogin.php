32bbf6bf46cc20db1952c5f4652badf465e1fae9
mlynto <127286780+mlynto@users.noreply.github.com>
2023-03-21 22:38:02 +0800

    Update mainlogin.php

--- mainlogin.php
+++ mainlogin.php
@@ -145,7 +145,7 @@
         <h1 id="info-title">&nbsp;&nbsp;&nbsp;Making Car Rental<br /> Easier For Customers</h1>
 
      
-        <form id = "login-form" onsubmit="return false" >
+        <form id = "login-form" action="checklogin.php" method="POST">
         <label for="username" style="font-style:italic;font-family: 'Roboto';color: #636363;font-size:1.1em;font-weight:bold;position:relative;top:11.5%;left:4.5%;"><img width="9.5%" height="9.5%" style="position:relative;top:1.5%;left:1.8%;"src="assets/user-filled.svg"></img>Username</label>
         <input id="username-input" placeholder="enter username" name="username"></input>
         <label for="password" id="labelpass" style="font-style:italic;position:relative;top:21.5%;left:14%;font-family: 'Roboto';color: #636363;font-size:1.1em;font-weight:bold;"><img width="8.5%" height="8.5%"src="assets/shield-lock.svg" style="position:relative;left:-93%;top:12.5%;">Password</label>
