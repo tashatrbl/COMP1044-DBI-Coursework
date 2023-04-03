40923ffa393e684df5287c2734cf8e9a6a3d6be6
mlynto <127286780+mlynto@users.noreply.github.com>
2023-04-03 10:33:03 +0800

    Update deleteprocess.php

--- deleteprocess.php
+++ deleteprocess.php
@@ -6,10 +6,16 @@ $Reservation_id=$_POST['ReservationID'];
 $sql = "DELETE FROM reservation WHERE reservation_id ='$Reservation_id'"; 
 $result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-	echo "Record deleted successfully";
-} else {
-	echo "Error deleting record: " . $conn->error;
+if ($conn->query($sql) === TRUE) 
+{
+	print "<script>alert('This reservation deleted successfully!'); 
+	window.location='delete-reservation2.php'</script>";
+}
+else
+{
+	print"<script> alert('The reservation ID is not valid. Please check again.');
+	window.location='delete-reservation2.php'</script>";
+
 }
 
 mysqli_close($conn);
