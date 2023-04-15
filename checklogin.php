85282c8f5258cc3adf37fe195b03cc3604c97f50
tashatrbl <carmelbarnabas@gmail.com>
2023-04-16 05:37:33 +0800

    Combined checklogin-new

--- checklogin.php
+++ checklogin.php
@@ -1,26 +1,33 @@
-<?php 
-    require('config.php');
+<?php
+require('config.php');
 
-    if(isset($_POST)){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+if (isset($_POST)) {
+    session_start();
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password_encrypted = openssl_encrypt($password, "AES-128-CTR", "comp1044", 0, '1111111111111000');
 
-       
-        $data = mysqli_query($conn,"SELECT * From staff WHERE username='$username' and password='$password'");
+    //code to show encrypted password entered on form
+    $jsc = "alert('" . $password_encrypted  . "')";
+    echo "<script>" . $jsc . "</script>";
 
-        $num_rows = mysqli_num_rows($data);
-        
-        if($num_rows>0)
-        {   
-            print "<script>alert('Welcome admin!');
+    $data = mysqli_query($conn, "SELECT * From staff WHERE username ='$username' and password = '$password_encrypted'");
+    $row = mysqli_fetch_assoc($data);
+    $actual_name = $row['actual_name'];
+
+    $num_rows = mysqli_num_rows($data);
+
+    if ($num_rows > 0) {
+        $_SESSION['username'] = $username;
+        $_SESSION['actual_name'] = $actual_name;
+        $_SESSION['logout'] = false;
+        print "<script>alert('Welcome " . $actual_name . "!');
             window.location= 'dashboard.php'</script>";
-         }
-        else
-        {
+    } else {
 
-            print"<script> alert('Username or password is incorrect! Please enter again.');
+        print "<script> alert('Username or password is incorrect! Please enter again.');
             window.location='mainlogin.php'</script>";
+    }
+}
 
-    }
-    }
-?>
