721c2118776dbeac801183aca8cc4c46a7a788a6
tashatrbl <carmelbarnabas@gmail.com>
2023-04-16 05:38:42 +0800

    Fixed small typo

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dbname = "comp1044_database";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
