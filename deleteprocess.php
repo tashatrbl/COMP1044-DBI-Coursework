158e19cc6ed8f787ae01b5bcbe30cc95e9c6d48d
mlynto <127286780+mlynto@users.noreply.github.com>
2023-04-17 14:19:46 +0800

    Update deleteprocess.php

--- deleteprocess.php
+++ deleteprocess.php
@@ -1,22 +1,22 @@
 <?php
 require('config.php');
 
-$reservationid = $_POST['reservationid'];
+$selectedReservations = $_POST['selected'];
+$reservationIds = implode(',', $selectedReservations);
 
-$sqldelete=mysqli_query($conn,"DELETE FROM reservation WHERE reservation_id ='$reservationid'");
+$sqldelete = mysqli_query($conn, "DELETE FROM reservation WHERE reservation_id IN ($reservationIds)");
 
 if ($sqldelete) 
 {
-	print "<script>alert('This reservation deleted successfully!'); 
-	window.location='delete-reservation2.php'</script>";
+    print "<script>alert('Selected reservations deleted successfully!'); 
+    window.location='delete-reservation2.php'</script>";
 }
 else
 {
-	print"<script> alert('The reservation ID is not valid. Please check again.');
-	window.location='delete-reservation2.php'</script>";
+    print"<script> alert('An error occurred while deleting the selected reservations. Please try again.');
+    window.location='delete-reservation2.php'</script>";
 
 }
 
 mysqli_close($conn);
-
 ?>
